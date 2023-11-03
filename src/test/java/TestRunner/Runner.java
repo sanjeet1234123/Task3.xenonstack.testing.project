fa237649d46b3873ad9bf23dce6c4ab139242345
sanjeet1234123 <117520248+sanjeet1234123@users.noreply.github.com>
2023-11-04 02:03:46 +0530

    Update Runner.java

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "/home/xs391-sanjha/IdeaProjects/demo/src/test/java/Features",
+        features = "/home/xs391-sanjha/IdeaProjects/xenonstack.testing.project/src/test/java/Features",
         glue="StepDefinition",
         plugin = {"pretty","html:target/HtmlReports/html-report.html"}
 )
